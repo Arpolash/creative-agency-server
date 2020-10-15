cab8015c55e5c3d44e5ee5d0fd995e44aa619a7b
arpolash <Arpolas2019@gmail.com>
2020-10-15 14:24:33 +0600

    api edited

--- index.js
+++ index.js
@@ -138,8 +138,8 @@ client.connect(err => {
     app.post('/isAdmin', (req, res) => {
       const email = req.body.email;
       adminCollection.find({ email: email })
-          .toArray((err, doctors) => {
-              res.send(doctors.length > 0);
+          .toArray((err, document) => {
+              res.send(document.length > 0);
           })
   })
 
